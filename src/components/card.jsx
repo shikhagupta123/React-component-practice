ecb9c453ddd71ead45c8056e6b424411555313a1
shikhagupta123 <52396979+shikhagupta123@users.noreply.github.com>
2021-07-04 23:36:37 +0530

    detailed-components

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Detail from "./Detail";
+import Avatar from "./Avatar";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.imgURL} alt="avatar_img" />
+        <Avatar imgURL={props.imgURL} />
       </div>
       <div className="bottom">
-        <p className="info">{props.phone}</p>
-        <p className="info">{props.email}</p>
+        <Detail info={props.phone} />
+        <Detail info={props.email} />
       </div>
     </div>
   );
